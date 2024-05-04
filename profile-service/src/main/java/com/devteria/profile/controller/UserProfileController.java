97c63b4639ca4d019c8243a47075fdcc529178cc
devteria <devteriadotcom@gmail.com>
2024-05-04 13:02:05 +0700

    config api gateway

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -1,6 +1,5 @@
 package com.devteria.profile.controller;
 
-
 import com.devteria.profile.dto.request.ProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
 import com.devteria.profile.service.UserProfileService;
@@ -24,11 +23,6 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserProfileController {
     UserProfileService userProfileService;
-
-    @PostMapping("/users")
-    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request) {
-        return userProfileService.createProfile(request);
-    }
 
     @GetMapping("/users/{profileId}")
     UserProfileResponse getProfile(@PathVariable String profileId) {
